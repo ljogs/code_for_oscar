80569394c28f1691612143deb11cc950f9bfada1
ljogs <jinyuan.otz@gmail.com>
2022-08-26 03:32:54 +0100

    Add files via upload

---
+++ load_simulation_data6.py
@@ -0,0 +1,313 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 16:11:15 2022
+
+@author: LJOGS
+"""
+
+
+import numpy as np
+import random
+from group_lasso import GroupLasso
+from sklearn.metrics import r2_score
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.linear_model import Lasso, ElasticNet, Ridge
+
+import pandas as pd
+
+
+import matlab.engine
+eng = matlab.engine.start_matlab()
+
+random.seed(10)
+seed_pool = random.sample(range(1,1000),100)
+
+OSCAR_MSE_log=[];
+Lasso_MSE_log=[];
+Elastic_net_MSE_log=[];
+GL_MSE_log=[];
+OSCAR_df_log=[];
+Lasso_df_log=[];
+Elastic_net_df_log=[];
+GL_df_log=[];
+clf_MSE_log=[];
+clf_df_log=[];
+
+percentage = 0;
+
+for random_seed in seed_pool:
+    
+    
+    percentage=percentage+1;
+    
+    num_samples = 70
+    p=40
+
+    mu = np.zeros(p)
+
+    Temp = np.mat(np.identity(p))
+
+
+    for i in range(p):
+        for j in range(p):
+            if i==j:
+                Temp[i,j]=1
+            else:
+                Temp[i,j]=0
+
+    rng = np.random.default_rng(random_seed)
+    cordataX = rng.multivariate_normal(mu, Temp, size=num_samples) 
+    
+    temprandom = np.random.normal(0,0.16,num_samples);
+    
+    for i in range(5):
+        cordataX[:,i]=cordataX[:,i]+temprandom
+
+    temprandom = np.random.normal(0,0.16,num_samples);
+
+    for i in range(5,10):
+        cordataX[:,i]=cordataX[:,i]+temprandom
+        
+    temprandom = np.random.normal(0,0.16,num_samples);
+        
+    for i in range(10,15):
+        cordataX[:,i]=cordataX[:,i]+temprandom
+
+    beta=np.array([3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+
+
+    groups = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    
+    np.random.seed(random_seed)
+    cordataY=np.dot(cordataX, beta)+np.random.normal(0,15,num_samples)
+
+    std_X = [];
+    for i in range(p):
+        std_X.append(np.std(cordataX[:,i]))
+    
+    std_X = np.array(std_X);
+    std_Y = np.std(cordataY)
+    
+    std_beta = std_X*beta/std_Y
+
+    for i in range(p):
+        cordataX[:,i] = (cordataX[:,i]-np.mean(cordataX[:,i]))/np.std(cordataX[:,i])
+        
+    cordataY=(cordataY-np.mean(cordataY))/np.std(cordataY)
+        
+
+    for i in range(p):
+        cordataX[:,i] = (cordataX[:,i]-np.mean(cordataX[:,i]))/np.std(cordataX[:,i])
+        
+    cordataY=(cordataY-np.mean(cordataY))/np.std(cordataY)
+
+
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test =  train_test_split(cordataX, cordataY, test_size=0.2, random_state=random_seed)
+
+
+    ####################################OSCAR###############
+ 
+    OSCAR_X_train = pd.DataFrame(X_train);
+    OSCAR_X_test = pd.DataFrame(X_test);
+    OSCAR_y_train = pd.DataFrame(y_train);
+    OSCAR_y_test = pd.DataFrame(y_test);
+    
+    OSCAR_X_train.to_csv('OSCAR_X_train.csv', index=False, header=False);
+    OSCAR_X_test.to_csv('OSCAR_X_test.csv', index=False, header=False);
+    OSCAR_y_train.to_csv('OSCAR_y_train.csv', index=False, header=False);
+    OSCAR_y_test.to_csv('OSCAR_y_test.csv', index=False, header=False);
+
+
+    ####################################OSCAR###############
+    
+    
+    oscar_coef, df= eng.oscar_package(nargout=2)
+    
+    oscar_coef=np.array(oscar_coef)
+    oscar_coef=oscar_coef[:,0];
+    oscar_coef = std_Y*oscar_coef/std_X;
+    oscar_mse = np.dot(np.dot(oscar_coef-beta, Temp),(oscar_coef-beta))
+    
+    oscar_mse = np.dot(np.dot(oscar_coef.T-beta, Temp),(oscar_coef.T-beta).T)
+    
+    OSCAR_MSE_log.append(oscar_mse[0,0])
+    OSCAR_df_log.append(df)
+    
+    #########################RIDGE#########################
+    
+    alphas = np.arange(0.01,1.5,0.1)
+    alphas = alphas*p;
+
+    coefs = []
+    errors =[]
+
+    for a in alphas:
+        clf = Ridge(alpha=a)
+        clf.fit(X_train, y_train)
+        coefs.append(clf.coef_)
+        errors.append(clf.score(X_test, y_test))
+
+    maxerror = np.max(errors)
+    maxindex = np.where(errors == maxerror)
+    optuning = alphas[maxindex]
+
+
+    clf.set_params(alpha=optuning[0])
+    clf.fit(X_train, y_train)
+    clf_coef = clf.coef_
+    clf_coef = std_Y*clf_coef/std_X;
+    clf_mse = np.dot(np.dot(clf_coef.T-beta.T, Temp),clf_coef-beta)
+    
+    clf_MSE_log.append(clf_mse[0,0])
+    clf_df_log.append(len(set(clf_coef[np.nonzero(clf_coef)])))
+    
+    
+    
+    #############################################LASSO######
+    
+    alphas = np.arange(0.01,1.3,0.05)
+    lasso = Lasso(max_iter=100)
+    coefs = []
+    errors =[]
+
+    for a in alphas:
+        lasso.set_params(alpha=a)
+        lasso.fit(X_train, y_train)
+        coefs.append(lasso.coef_)
+        errors.append(lasso.score(X_test, y_test))
+
+    maxerror = np.max(errors)
+    maxindex = np.where(errors == maxerror)
+    optuning = alphas[maxindex]
+
+
+    lasso.set_params(alpha=optuning[0])
+    lasso.fit(X_train, y_train)
+    lasso_coef = lasso.coef_
+    lasso_coef = std_Y*lasso_coef/std_X;
+    lasso_mse = np.dot(np.dot(lasso_coef.T-beta.T, Temp),lasso_coef-beta)
+    
+    Lasso_MSE_log.append(lasso_mse[0,0])
+    Lasso_df_log.append(len(set(lasso_coef[np.nonzero(lasso_coef)])))
+    
+    #######################################################
+    
+    
+    alphas1 = np.arange(0.01,1,0.05)
+    alphas2 = np.arange(0.01,1.5,0.05)
+    elnet = ElasticNet(random_state=0)
+    coefs = []
+    errors =[]
+    optuning=np.array([0,0])
+    besterror = -100
+
+    for a in alphas1:
+        for b in alphas2:
+            elnet.set_params(alpha=a+b, l1_ratio=a/(a+b))
+            elnet.fit(X_train, y_train)
+            coefs.append(elnet.coef_)
+            temperror = elnet.score(X_test, y_test)
+            if temperror > besterror:
+                besterror = temperror
+                optuning=np.array([a,b])
+        
+
+
+    elnet.set_params(alpha=optuning[0]+optuning[1], l1_ratio=optuning[0]/(optuning[0]+optuning[1]))
+    elnet.fit(X_train, y_train)
+    en_coef = elnet.coef_
+    en_coef = std_Y*en_coef/std_X;
+    en_mse = np.dot(np.dot(en_coef.T-beta.T, Temp),en_coef-beta)
+    
+    Elastic_net_MSE_log.append(en_mse[0,0])
+    Elastic_net_df_log.append(len(set(en_coef[np.nonzero(en_coef)])))
+    
+##########################################################################        
+    alphas = np.arange(0.01,1,0.01)
+    betas = np.arange(0.01,0.1,0.01)
+    coefs = []
+    errors =[]
+    tuning = []
+
+
+    for a in alphas:
+        for b in betas:
+            gl = GroupLasso(
+                groups=groups,
+                group_reg=b,
+                l1_reg=a,
+                frobenius_lipschitz=True,
+                scale_reg="none",
+                subsampling_scheme=1,
+                supress_warning=True,
+                n_iter=100,
+                tol=1e-3,
+            )
+            gl.fit(X_train, y_train)
+            yhat = gl.predict(X_test)
+            errors.append(r2_score(y_test, yhat))
+            coefs.append(gl.coef_)
+            tuning.append(np.array([a, b]))
+
+
+    minerror = np.max(errors)
+    minindex = np.where(errors == minerror)
+    minindex = np.array(minindex)[0,0]
+    gl_coef = coefs[minindex]
+    gl_coef = gl_coef[:,0]
+    gl_coef = std_Y*gl_coef/std_X;
+
+    gl_mse = np.dot(np.dot(gl_coef-beta, Temp),gl_coef-beta)
+    
+    GL_MSE_log.append(gl_mse[0,0])
+    GL_df_log.append(len(set(gl_coef[np.nonzero(gl_coef)])))
+    
+    print(percentage, '%');
+
+
+np.savetxt('OSCAR_MSE_log6.txt', OSCAR_MSE_log)
+
+np.savetxt('Lasso_MSE_log6.txt', Lasso_MSE_log)
+
+np.savetxt('Elastic_net_MSE_log6.txt', Elastic_net_MSE_log)
+
+np.savetxt('GL_MSE_log6.txt', GL_MSE_log)
+
+np.savetxt('OSCAR_df_log6.txt', OSCAR_df_log)
+
+np.savetxt('Lasso_df_log6.txt', Lasso_df_log)
+
+np.savetxt('Elastic_net_df_log6.txt', Elastic_net_df_log)
+
+np.savetxt('GL_df_log6.txt', GL_df_log)
+
+np.savetxt('clf_MSE_log6.txt', clf_MSE_log)
+
+np.savetxt('clf_df_log6.txt', clf_df_log)
+
+
+print('OSCAR MSE:', np.mean(OSCAR_MSE_log))
+print('Ridge MSE:', np.mean(clf_MSE_log))
+print('Lasso MSE:', np.mean(Lasso_MSE_log))
+print('Elastic Net MSE:', np.mean(Elastic_net_MSE_log))
+print('Group Lasso MSE:', np.mean(GL_MSE_log))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ oscar_package.m
@@ -0,0 +1,79 @@
+ 
+
+% X should be a matrix whose rows are the observations and columns are the
+% predictors (n by p). The intercept is omitted (so the response and each
+% predictor should have mean zero).
+
+function [op_coef, df] = oscar_package()
+
+format long
+
+X_train = readmatrix("OSCAR_X_train.csv");
+X_test = readmatrix("OSCAR_X_test.csv");
+y_train = readmatrix("OSCAR_y_train.csv");
+y_test = readmatrix("OSCAR_y_test.csv");
+
+cvalues = [0; .01; .05; .1;.25;.5;.55;.6;.7;.75;.8;.85;.9;.95;1];
+propvalues = [.0001; .001; .002; .00225; .0025; .00275; .0028; .0029; .003; .004; .005; .0075; .01;.025; .05; .1; .15;.2;.25;.3;.35;.4;.45;.5;.55;.6];
+
+p = length(X_train(1,:));
+
+[initcoef] = regress(y_train,X_train); 
+
+Xmatrix = [X_train -X_train]; 
+
+[initcoeford, currorder] = sort(-abs(initcoef));
+sameaslast = [0; (initcoeford(2:p) == initcoeford(1:(p-1)))];
+startblocksame = [((sameaslast(2:p) - sameaslast(1:(p-1))) > 0); 0];
+
+endblocksame = [((sameaslast(2:p) - sameaslast(1:(p-1))) < 0); sameaslast(p)];
+nblocksame = sum(startblocksame);
+vi = (1:p)';
+visbs = vi(logical(startblocksame));
+viebs = vi(logical(endblocksame));
+for i = 1:nblocksame; 
+    blockmean = mean(vi(visbs(i):viebs(i)));
+    vi(visbs(i):viebs(i)) = blockmean * ones(viebs(i) - visbs(i) + 1,1);
+end;
+[tempinvsort,vind] = sort(currorder);
+a1 = vi(vind)';
+initcoeford = -initcoeford;   
+
+CoefMatrix = zeros(p,length(propvalues),length(cvalues));
+Test_Error_Matrix = zeros(length(propvalues),length(cvalues));
+SSMatrix = zeros(1,length(propvalues),length(cvalues));
+dfMatrix = zeros(1,length(propvalues),length(cvalues));
+
+for ccount = 1:length(cvalues)
+    OrderMatrix = a1;
+    weighting = a1;
+    cvalue = cvalues(ccount);
+    for i=1:p
+        weighting(i) = (1-cvalue)+cvalue*(p-i);
+    end;    
+    for propcount = 1:length(propvalues)
+        tbound = propvalues(propcount)*weighting*initcoeford;
+        if (ccount == 1)
+            if (propcount == 1)
+                start=zeros(2*p,1);
+            end;
+        elseif (propcount > 1)
+            start=[max(CoefMatrix(:,propcount-1,ccount),0);-min(CoefMatrix(:,propcount-1,ccount),0)];
+        else
+            start=[max(CoefMatrix(:,propcount,ccount-1),0);-min(CoefMatrix(:,propcount,ccount-1),0)];
+        end;
+        [coefs df ssquares conv] = OscarSeqOpt(tbound, cvalue, Xmatrix, y_train, start, OrderMatrix);
+        CoefMatrix(:,propcount,ccount) = coefs;
+        Test_Error_Matrix(propcount,ccount) = sum((X_test*coefs-y_test).^2);
+        SSMatrix(:,propcount,ccount) = ssquares;
+        dfMatrix(:,propcount,ccount) = df;
+    end;
+end;
+
+[op_row, op_column]=find(Test_Error_Matrix==min(min(Test_Error_Matrix)));
+op_row = op_row(1);
+op_column = op_column(1);
+df = dfMatrix(:,op_row, op_column);
+
+op_coef=CoefMatrix(:,op_row, op_column);
+
